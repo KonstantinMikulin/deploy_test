a934c8dd2735eb4903ef7cd85a4f697b8c99952a
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2024-04-30 18:21:43 +0300

    Logging config

--- logging_config_example/main.py
+++ logging_config_example/main.py
@@ -2,8 +2,10 @@ import logging
 
 from module_1 import main
 
+# Настраиваем базовую конфигурацию логирования
 logging.basicConfig(
     format='#%(levelname)-8s %(name)s:%(funcName)s - %(message)s'
 )
 
+# Исполняем функцию `main` из модуля `module_1.py`
 main()
--- logging_config_example/module_1.py
+++ logging_config_example/module_1.py
@@ -3,27 +3,40 @@ import logging
 from module_2 import devide_number
 from module_3 import square_number
 
+# Инициализируем логгер модуля
 logger = logging.getLogger(__name__)
+
+# Устанавливаем логгеру уровень `DEBUG`
 logger.setLevel(logging.DEBUG)
 
 
+# Определяем свой фильтр, наследуясь от класса Filter библиотеки logging
 class ErrorLogFilter(logging.Filter):
+    # Переопределяем метод filter, который принимает `self` и `record`
+    # Переменная рекорд будет ссылаться на объект класса LogRecord
     def filter(self, record):
         return record.levelname == 'ERROR'
 
 
+# Инициализируем форматтер
 formatter_1 = logging.Formatter(
     fmt='[%(asctime)s] #%(levelname)-8s %(filename)s:'
         '%(lineno)d - %(name)s:%(funcName)s - %(message)s'
 )
 
+# Инициализируем хэндлер, который будет писать логи в файл `error.log`
 error_file = logging.FileHandler('error.log', 'w', encoding='utf-8')
+# Устанавливаем хэндлеру уровень `DEBUG`
 error_file.setLevel(logging.DEBUG)
 
+# Добавляем хэндлеру фильтр `ErrorLogFilter`, который будет пропускать в
+# хэндлер только логи уровня `ERROR`
 error_file.addFilter(ErrorLogFilter())
 
+# Определяем форматирование логов в хэндлере
 error_file.setFormatter(formatter_1)
 
+# Добавляем хэндлер в логгер
 logger.addHandler(error_file)
 
 
@@ -38,4 +51,4 @@ def main():
     logger.critical('Лог CRITICAL')
 
     print(devide_number(a, square_number(b)))
-    print(devide_number(c, square_number(d)))
+    print(devide_number(square_number(c), d))
