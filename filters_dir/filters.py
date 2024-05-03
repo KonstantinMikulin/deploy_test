9ee88e8a6e7929b8fb7b9f01b2415ae6e04e8221
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2024-05-03 15:10:52 +0300

    Test from scratch main.py. Filters for checking admins ID

--- filters_dir/filters.py
+++ filters_dir/filters.py
@@ -1,20 +1,16 @@
 import logging
 
 from aiogram.filters import BaseFilter
-from aiogram.types import TelegramObject
+from aiogram.types import TelegramObject, Message
+
+from config_data.config_2 import admin_id
 
 logger = logging.getLogger(__name__)
 
 
-class MyTrueFilter(BaseFilter):
-
-    async def __call__(self, event: TelegramObject) -> bool:
-        logger.debug('Попали внутрь фильтра %s', __class__.__name__)
-        return True
-
-
-class MyFalseFilter(BaseFilter):
-    async def __call__(self, event: TelegramObject) -> bool:
-        logger.debug('Попали внутрь фильтр %s', __class__.__name__)
-
-        return False
+class CheckAdminFilter(BaseFilter):
+    async def __call__(self, event: TelegramObject, message: Message) -> bool:
+        if message.from_user.id in admin_id:
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -8,3 +8,11 @@ from aiogram.enums import ParseMode
 from config_data.config import Config, load_config
 from handlers_dir.user_handlers import user_router
 from handlers_dir.other_handlers import other_router
+
+from middlewares_dir.outer_middlewares import SomeOuterMiddleware
+
+
+def main():
+    dp: Dispatcher = Dispatcher()
+
+    dp.message.outer_middleware(SomeOuterMiddleware())
