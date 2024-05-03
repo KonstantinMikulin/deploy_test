5f6fc2445e914d21770a3525ecc06b86c3ee0394
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2024-05-03 15:14:56 +0300

    Purge most of old code and minor fix in filters.py

--- filters_dir/filters.py
+++ filters_dir/filters.py
@@ -10,6 +10,8 @@ logger = logging.getLogger(__name__)
 
 class CheckAdminFilter(BaseFilter):
     async def __call__(self, event: TelegramObject, message: Message) -> bool:
+        logger.debug('Вошли в фильтр %s', __class__.__name__)
+
         if message.from_user.id in admin_id:
             return True
         else:
