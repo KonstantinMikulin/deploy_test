9006c2f79c6dc67f877ebf30889e679a90f3e4c3
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2024-05-10 14:38:11 +0300

    aiogram_dialog test. Initial.

--- test.py
+++ test.py
@@ -1,30 +1,5 @@
-import logging
-import asyncio
+from datetime import datetime
 
-from aiogram import Bot, Dispatcher
-from config_data.config import Config, load_config
-from handlers_dir.user_handlers import user_router
+res = datetime.now()
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='[%(asctime)s] #%(levelname)-8s %(filename)s:'
-           '%(lineno)d - %(name)s - %(message)s'
-)
-
-# Инициализируем логгер модуля
-logger = logging.getLogger(__name__)
-
-
-async def main():
-    config: Config = load_config()
-    bot = Bot(token=config.tg_bot.token)
-    dp = Dispatcher()
-
-    dp.include_router(user_router)
-
-    await dp.start_polling(bot)
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
-
+print(type(res))
