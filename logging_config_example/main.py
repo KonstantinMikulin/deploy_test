be6c79f788e2de316ae5f45481281ce8053a8071
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-08-11 16:00:07 +0300

    deploy test new bot

--- logging_config_example/main.py
+++
@@ -1,8 +0,0 @@
-import logging.config
-
-from logging_settings import logging_config
-from module_1 import main
-
-logging.config.dictConfig(logging_config)
-
-main()
--- test_logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from logging_config_example.module_1 import main
-
-logging.basicConfig(
-    format='#%(levelname)-8s %(name)s:%(funcName)s - %(message)s'
-)
-
-main()
