2830bc008c42a052be661090623e2bc84bc99cb7
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-08-13 17:20:35 +0300

    change files structure

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import asyncio
 
 from aiogram import Bot, Dispatcher # type: ignore
 
-from bot.config_reader import Config, load_config
-from bot.handlers import get_routers
+from config_reader import Config, load_config
+from handlers import get_routers
 
 
 async def main() -> None:
