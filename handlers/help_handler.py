8cfd67275a5bda74b845063a6c5098b950e7fd7f
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-08-15 15:15:05 +0300

    add /desc handler

--- handlers/help_handler.py
+++ handlers/help_handler.py
@@ -8,3 +8,8 @@ router = Router()
 @router.message(Command(commands=['help']))
 async def process_help_cmd(message: Message) -> None:
     await message.answer('You sent /help')
+
+
+@router.message(Command(commands=['desc']))
+async def process_desc_cmd(message: Message) -> None:
+    await message.answer('Some text after /desc command')
