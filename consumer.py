c31e68573e5b9767afeec9dfda69467268329bae
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-09-04 19:52:31 +0300

    add consumer for rabbit mq

---
+++ consumer.py
@@ -0,0 +1,57 @@
+import asyncio
+import json
+import time
+
+import aiormq
+from aiormq.abc import DeliveredMessage
+
+
+# Callbck-функция, вызываемая на каждое сообщение для консьюмера
+async def on_message(message: DeliveredMessage):
+    # Декодируем и десериализируем сообщение
+    body: dict = json.loads(message.body.decode('utg-8'))
+    
+    print(f'Сообщение {body.get("counter")} получено')
+    print(f'{body.get("text")} {body.get("counter")}')
+    
+    # Имитируем выполнение тяжелой CPU-бонд задачи
+    time.sleep(body.get('delay'))  # type:ignore
+    
+    # Явно подтверждаем получение и обработку сообщения
+    await message.channel.basic_ack(delivery_tag=message.delivery.delivery_tag)  # type:ignore
+    print(f'Сообщение {body.get("counter")} обработано')
+    
+    
+async def main():
+    # Подключаемся к RabbitMQ
+    connection = await aiormq.connect(
+        "amqp://costa:12345@localhost/"
+    )
+
+    # Создаем канал
+    channel = await connection.channel()
+    
+    # Объявляем точку обмена (создается, если не существует)
+    await channel.exchange_declare('test_exchange', exchange_type='direct')
+    
+    # Объявляем очередь (создается, если не существует)
+    declare_ok = await channel.queue_declare('test_queue')
+    
+    # Привязываем очередь к точке обмена
+    await channel.queue_bind(
+        queue=declare_ok.queue,
+        exchange="test_exchange",
+        routing_key="test_routing_key"
+    )
+    
+    # Определяем количество сообщений, которое консьюмер может получить за один раз
+    # await channel.basic_qos(prefetch_count=1)
+    
+    # Настраиваем прослушивае очереди
+    await channel.basic_consume(declare_ok.queue, on_message, no_ack=False)
+    
+    # Создаем бесконечный цикл для ожидание сообщений для консьюмера из очереди
+    await asyncio.Future()
+    
+    
+asyncio.run(main())
