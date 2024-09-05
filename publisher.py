bb8f145f83fded4c24cc1f1d3be97f2ce4ea766f
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-09-05 17:15:13 +0300

    success test rabbitmq delayed massages

--- publisher.py
+++ publisher.py
@@ -10,34 +10,37 @@ async def publish_message():
     connection = await aiormq.connect(
         "amqp://costa:12345@localhost/"
     )
-    
+
     # Создаем канал
     channel = await connection.channel()
-    
+
     # Объявляем точку обмена (создается, если не существует)
-    await channel.exchange_declare('main_exchange', exchange_type='direct')
-    
-    # Объявляем время, когда сообщение должно быть обработано
-    scheduled_time = (datetime.now(timezone.utc) + timedelta(seconds=5)).isoformat()
-    
+    await channel.exchange_declare("main_exchange", exchange_type="direct")
+
+    # Определяем время, когда сообщение должно быть обработано
+    scheduled_time = (
+        datetime.now(timezone.utc) + timedelta(seconds=5)
+    ).isoformat()
+
     # Создаем словарь, из которого будет сформировано тело сообщения
     body = {
-        'text': 'Hi from RabbirMQ!'
+        "text": "Привет из RabbitMQ!",
     }
-    
-    # Отправляем сообщение в обменник main_exchange
+
+    # Отправляем сообщение в `main_exchange`
     await channel.basic_publish(
         body=json.dumps(body).encode("utf-8"),
         exchange="main_exchange",
         routing_key="main_routing_key",
         properties=aiormq.spec.Basic.Properties(
-            headers={
-                "scheduled_time": scheduled_time
-            }
+            headers={"scheduled_time": scheduled_time}
         ),
     )
-    
-    print(f'Published message with scheduled time: {scheduled_time}')
-    
-    
+
+    print(f"Published message with scheduled time: {scheduled_time}")
+
+    # Закрываем соединение
+    await connection.close()
+
+
 asyncio.run(publish_message())
