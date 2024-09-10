1fc467e6619553c2b390f31c453f80ad91fa1337
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-09-10 10:18:22 +0300

    add nats servers

--- config_data/config.py
+++ config_data/config.py
@@ -5,14 +5,23 @@ from environs import Env
 @dataclass
 class TgBot:
     token: str  # Токен для доступа к телеграм-боту
+    
+    
+@dataclass
+class NatsConfig:
+    servers: list[str]
 
 
 @dataclass
 class Config:
     tg_bot: TgBot
+    nats: NatsConfig
 
 
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
-    return Config(tg_bot=TgBot(token=env("BOT_TOKEN")))
+    return Config(
+        tg_bot=TgBot(token=env("BOT_TOKEN")),
+        nats=NatsConfig(servers=env.list("NATS_SERVERS")),
+    )
