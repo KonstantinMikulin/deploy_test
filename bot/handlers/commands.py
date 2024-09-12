1b92ac9c934dd47c6b3c03e65c1571939bebaec8
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-09-12 17:19:03 +0300

    minor change

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -2,7 +2,6 @@ from aiogram import Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from sqlalchemy import insert, delete, select, column
-from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio.engine import AsyncEngine
 
 from bot.db.tables import users as users_table
