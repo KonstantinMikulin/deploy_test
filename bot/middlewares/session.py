b7a057e6402dd6fc4373c84ef7f5e2f90f75dd6b
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-10-23 20:31:08 +0300

    save

--- bot/middlewares/session.py
+++ bot/middlewares/session.py
@@ -1,6 +1,6 @@
 from typing import  Callable, Awaitable, Dict, Any
 
-from aiogram import  BaseMiddleware
+from aiogram import  BaseMiddleware #
 from aiogram.types import TelegramObject
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
