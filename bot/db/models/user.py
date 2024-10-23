42d65ee76418c9f5175b2a7b8fb7bc0ce8aeeaf8
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-10-23 15:25:57 +0300

    add user model for db

---
+++ bot/db/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import BigInteger, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from bot.db import Base
+from bot.db.models.mixins import TimestampMixin
+
+
+class User(TimestampMixin, Base):
+    __tablename__ = 'users'
+    
+    telegram_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    first_name: Mapped[str] = mapped_column(String, nullable=False)
+    last_name: Mapped[str | None] = mapped_column(String, nullable=True)
+    
+    games: Mapped[list['Game']] = relationship(back_populates='user')
