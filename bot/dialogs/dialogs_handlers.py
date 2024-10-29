92cb35ae1e959c8cdd2e244799cfd2b6e350272f
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-10-29 16:09:00 +0300

    add handlers for correct weigh and handler for weight error

--- bot/dialogs/dialogs_handlers.py
+++ bot/dialogs/dialogs_handlers.py
@@ -1,3 +1,9 @@
+from aiogram.types import Message
+
+from aiogram_dialog import DialogManager
+from aiogram_dialog.widgets.input import ManagedTextInput
+
+
 def validate_weight(text: str) -> str | None:
     weight = round(float(text), 2)
     if 20 <= weight <= 500:
@@ -6,3 +12,26 @@ def validate_weight(text: str) -> str | None:
     raise ValueError
 
 
+async def weight_correct_handler(
+    message: Message,
+    widget: ManagedTextInput,
+    dialog_manager: DialogManager,
+    text: str,
+) -> None:
+    weight = round(float(text), 2)
+    dialog_manager.dialog_data["initial_weight"] = weight
+
+    # TODO: add html.escape()
+    await message.answer(
+        f"Your current weight is {weight}\nYou will achieve your goals!"
+    )
+    await dialog_manager.reset_stack()
+
+
+async def weight_error_handler(
+    message: Message,
+    widget: ManagedTextInput,
+    dialog_manager: DialogManager,
+    text: str,
+) -> None:
+    await message.answer("The weight must be int or float")
--- bot/dialogs/weight_dialog.py
+++ bot/dialogs/weight_dialog.py
@@ -1,9 +1,9 @@
 from aiogram_dialog import Dialog, Window
 from aiogram_dialog.widgets.text import Const
-from aiogram_dialog.widgets.input import TextInput, MessageInput
+from aiogram_dialog.widgets.input import TextInput
 
 from . states import AddWeightSG
-from . dialogs_handlers import validate_weight
+from . dialogs_handlers import validate_weight, weight_correct_handler
 
 
 add_weight_dialog = Dialog(
@@ -12,10 +12,9 @@ add_weight_dialog = Dialog(
         TextInput(
             id="fill_weight",
             type_factory=validate_weight,
-            on_success=weight_fill_correct_handler,  # type: ignore
+            on_success=weight_correct_handler,  # type: ignore
             on_error=weight_error_handler,  # type: ignore
         ),
-        Button(Const("Cancel"), id="cancel_fill", on_click=cancel_fill_profile),
-        state=FillProfileSG.fill_init_weight,
+        state=AddWeightSG.add_weight
     )
 )
