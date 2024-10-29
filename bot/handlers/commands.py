b1490fdaffe7e1278f80c52f7787ac808d175df7
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-10-29 16:18:25 +0300

    can fun without adding data to db

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -7,8 +7,8 @@ from aiogram_dialog import DialogManager, StartMode, ShowMode
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from bot.db.requests import add_weight, get_total_weight_for_user
-from bot.agrm_dialogs import add_weight_dialog
-from bot.agrm_dialogs.states import AddWeightSG
+from bot.dialogs import add_weight_dialog
+from bot.dialogs.states import AddWeightSG
 
 router = Router(name='commands router')
 
@@ -21,14 +21,14 @@ async def cmd_start(message: Message):
 @router.message(Command('add'))
 async def cmd_add_weight(message: Message, dialog_manager: DialogManager, session: AsyncSession):
     await dialog_manager.start(state=AddWeightSG.add_weight, mode=StartMode.RESET_STACK, show_mode=ShowMode.AUTO)
-    weight = dialog_manager.dialog_data.get('weight')
+    # weight = dialog_manager.dialog_data.get('weight')
 
-    await add_weight(
-        session=session,
-        telegram_id=message.from_user.id,  #type:ignore
-        weight=weight,
-    )
-    await message.answer(f"You got {weight}")
+    # await add_weight(
+    #     session=session,
+    #     telegram_id=message.from_user.id,  #type:ignore
+    #     weight=weight,
+    # )
+    # await message.answer(f"You got {weight}")
 
 
 @router.message(Command('stats'))
